453642ab052b731e47abf51de613a7744f942b48
sadhubby <iamthegreatgamerx.silent@gmail.com>
2024-12-01 00:22:58 +0800

    can display html now

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -30,6 +31,10 @@ const queryAsync = (sql, params) => {
         });
     });
 };
+
+app.get('/', async (req, res) =>{
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
 
 // Fetch game details by AppID
 app.post('/get_game', async (req, res) => {
